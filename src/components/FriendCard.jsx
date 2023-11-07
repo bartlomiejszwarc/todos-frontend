a1e42d8369fb31f0b8684d61c9c87e738ba6327e
bartlomiejszwarc <szwarc.it@gmail.com>
2023-11-07 12:42:09 +0100

    Adjusted friend card size

--- src/components/FriendCard.jsx
+++ src/components/FriendCard.jsx
@@ -2,7 +2,7 @@ import { Avatar } from '@mui/material';
 function FriendCard({ friend }) {
   return (
     <>
-      <div className='flex flex-col items-center justify-center aspect-square w-[30%] md:w-[15%] border-2 rounded-lg leading-none m-1'>
+      <div className='flex flex-col items-center justify-center aspect-square w-[30%] xl:w-[20%] border-2 rounded-lg leading-none m-1'>
         <Avatar></Avatar>
         <span className='text-xl'>{friend.displayName} </span>
         <span>{friend.username}</span>
