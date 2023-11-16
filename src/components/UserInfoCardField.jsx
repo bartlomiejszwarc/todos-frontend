f83b08efc5f760efbad509b4205bee4ffdc8ef78
bartlomiejszwarc <szwarc.it@gmail.com>
2023-11-16 14:40:08 +0100

    Redesigned user info dialog

---
+++ src/components/UserInfoCardField.jsx
@@ -0,0 +1,13 @@
+function UserInfoCardField({ fieldTitle, fieldText }) {
+  return (
+    <div className='w-full flex flex-col m-0 font-quicksand'>
+      <p className='text-sm text-neutral-500'>{fieldTitle}</p>
+      {fieldText ? (
+        <p className='text-lg text-neutral-900 font-[600]'>{fieldText}</p>
+      ) : (
+        <p className='text-lg text-neutral-900 font-[600]'>Unavailable</p>
+      )}
+    </div>
+  );
+}
+export default UserInfoCardField;
--- src/pages/Friends/FriendContactInfo.jsx
+++ src/pages/Friends/FriendContactInfo.jsx
@@ -1,35 +1,25 @@
 import CustomAvatar from '../../components/CustomAvatar';
-import PhoneIcon from '@mui/icons-material/Phone';
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+import UserInfoCardField from '../../components/UserInfoCardField';
 function FriendContactInfo({ friend }) {
   return (
-    <div className='w-full py-16 flex flex-col items-center space-y-5'>
-      <div className='w-1/2 space-y-5'>
-        <div className='flex flex-col items-center '>
-          <CustomAvatar user={friend} size='5' />
-          <span className='text-2xl'>{friend?.displayName}</span>
-          <span className='text-base text-neutral-600'>{friend?.username}</span>
+    <div className='w-full py-16 flex flex-col items-center space-y-5 px-4 bg-neutral-200'>
+      <div className='h-auto bg-neutral-50 rounded-lg w-full lg:w-10/12 pb-6'>
+        <div className={'relative w-full h-10'}>
+          <div className='absolute flex top-0 translate-y-[-50%] justify-center w-full '>
+            <CustomAvatar user={friend} size={5} />
+          </div>
         </div>
-        <div className='space-y-3 flex flex-col items-center'>
-          <div className='w-full  flex justify-center text-2xl'>Contact</div>
-          <div className='space-y-1'>
-            <div className='flex space-x-2 '>
-              <PhoneIcon sx={{ color: '#16a34a' }} />
-              {friend?.phoneNumber ? (
-                <p className='font-[500]'>+{friend?.phoneNumber}</p>
-              ) : (
-                <p className='font-[300]'>Unavailable</p>
-              )}
-            </div>
-
-            <div className='flex space-x-2 '>
-              <AlternateEmailIcon sx={{ color: '#0284c7' }} />
-              {friend?.email ? (
-                <p className='font-[500]'>+{friend?.email}</p>
-              ) : (
-                <p className='font-[300]'>Unavailable</p>
-              )}
-            </div>
+        <div className='flex justify-around'>
+          <div className='flex flex-col space-y-3 px-6'>
+            <UserInfoCardField fieldTitle={'Name'} fieldText={friend?.displayName} />
+            <UserInfoCardField fieldTitle={'Username'} fieldText={friend?.username} />
+            <UserInfoCardField fieldTitle={'Phone number'} fieldText={friend?.phoneNumber} />
+            <UserInfoCardField fieldTitle={'E-mail address'} fieldText={friend?.email} />
+          </div>
+          <div className='flex flex-col space-y-3 px-6'>
+            <UserInfoCardField fieldTitle={'Country'} fieldText={friend?.country} />
+            <UserInfoCardField fieldTitle={'City'} fieldText={friend?.city} />
+            <UserInfoCardField fieldTitle={'Something'} fieldText={friend?.something} />
           </div>
         </div>
       </div>
